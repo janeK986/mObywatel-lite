f196e31acc7618fbf79e29fa77f8a5e2b286bd91
janeK986 <pawlatajan@gmail.com>
2025-09-14 21:50:56 +0200

    Update user-data.js

--- strona/assets/user-data.js
+++ strona/assets/user-data.js
@@ -33,28 +33,28 @@ export const userData = {
 export const userProfiles = {
     jan: {
         name: 'Jan',
-        surname: 'Kowalski',
+        surname: 'Pawlata',
         nationality: 'polskie',
-        birthday: '15.03.1985',
-        pesel: '85031512345',
-        seriesAndNumber: 'ABC123456',
-        expiryDate: '15.03.2030',
-        givenDate: '15.03.2020',
-        fathersName: 'Andrzej',
-        mothersName: 'Anna',
+        birthday: '09.08.2007',
+        pesel: '07280902957',
+        seriesAndNumber: 'ZZC 216937',
+        expiryDate: '15.03.2034',
+        givenDate: '15.03.2024',
+        fathersName: 'Artur',
+        mothersName: 'Renata',
         familyName: 'Kowalski',
         sex: 'M',
-        fathersFamilyName: 'Kowalski',
-        mothersFamilyName: 'Nowak',
-        birthPlace: 'Warszawa',
+        fathersFamilyName: 'Pawlata',
+        mothersFamilyName: 'Ledzion',
+        birthPlace: 'Łowicz',
         countryOfBirth: 'Polska',
-        adress: 'ul. Przykładowa 123, 00-001 Warszawa',
+        adress: 'ul. Ikara 2 , 99-400 Łowicz',
         homeDate: '01.01.2010'
     },
     
     anna: {
-        name: 'Anna',
-        surname: 'Nowak',
+        name: 'Maja',
+        surname: 'Kosiorek',
         nationality: 'polskie', 
         birthday: '22.07.1990',
         pesel: '90072212345',
@@ -72,7 +72,30 @@ export const userProfiles = {
         adress: 'ul. Testowa 456, 30-001 Kraków',
         homeDate: '15.05.2015'
     }
+},
+
+ filip: {
+        name: 'Filip',
+        surname: 'Żaczek',
+        nationality: 'polskie', 
+        birthday: '20.07.2007',
+        pesel: '07272002957',
+        seriesAndNumber: 'DEF789012',
+        expiryDate: '22.07.2035',
+        givenDate: '22.07.2025',
+        fathersName: 'Mariusz',
+        mothersName: 'Magdalena',
+        familyName: 'Żaczek',
+        sex: 'M',
+        fathersFamilyName: 'Żaczek',
+        mothersFamilyName: 'Słoma',
+        birthPlace: 'Skierniewice',
+        countryOfBirth: 'Polska',
+        adress: 'ul. Ikara 2, 99-400 Łowicz',
+        homeDate: '15.05.2017'
+    }
 };
 
 // Aktualnie wybrany profil (możesz zmienić)
+
 export const currentProfile = 'jan'; // lub 'anna'
